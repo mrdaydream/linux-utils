af5496d49a976274e495677348270b60ab51a7c7
cslcdr <15980905+cslcdr@users.noreply.github.com>
2021-03-02 10:15:44 +0000

    gg update

--- gg.sh
+++ gg.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo
-
 for f in $(find . -type d -name '.git'); do
   f=$(dirname $f)
-  echo '>>>' $f '<<<'; echo
-  git -C "$f" "$@"; echo
+  echo ; echo ; echo
+  echo '----------------------------------------'
+  echo '~>' $f
+  git -C "$f" "$@"
+  echo '----------------------------------------'
 done
+
+echo ; echo ; echo
